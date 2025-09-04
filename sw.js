558a7c110205d4c833268f84c4d5731a75015131
sevencode777-leys <lujainalnesser@gmail.com>
2025-09-05 00:37:30 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+
+const CACHE_NAME = 'elon-wealth-game-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
